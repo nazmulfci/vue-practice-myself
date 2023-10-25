bec4142588cc0f5b8d1d4933045748befb98704c
nazmulfci <nazmulfci@yahoo.com>
2023-10-26 01:01:33 +0600

    withOutPinia

--- pinia-state-management/src/store/store.js
+++ pinia-state-management/src/store/store.js
@@ -1,17 +1,27 @@
 import {defineStore} from 'pinia'
+import {ref,computed} from 'vue'
 
-const counterStore = defineStore('counter', {
-    state: () => ({
-        count: 10
-    }),
-    getters: {
-        doubleCount: (state) => state.count * 2
-    },
-    actions: {
-        increment() {
-            this.count++
-        }
+// const counterStore = defineStore('counter', {
+//     state: () => ({
+//         count: 10
+//     }),
+//     getters: {
+//         doubleCount: (state) => state.count * 2
+//     },
+//     actions: {
+//         increment() {
+//             this.count++
+//         }
+//     }
+// })
+
+const counterStore = defineStore('counter',() => {
+    const count = ref(10);
+    const doubleCount = computed(() => count.value * 2);
+    const increment = () => {
+        count.value++
     }
+    return {count,doubleCount,increment}
 })
 
 export {counterStore}
